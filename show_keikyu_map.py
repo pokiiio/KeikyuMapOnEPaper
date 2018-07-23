14656328e924f6ac0d4e21102e79627cc0e4c500
pokio <32353873+pokiiio@users.noreply.github.com>
2018-07-24 00:38:28 +0900

    Add header.

--- show_keikyu_map.py
+++ show_keikyu_map.py
@@ -2,12 +2,14 @@
 
 import numpy
 import epd2in7b
-from os import path
+import text_to_image
 from PIL import Image, ImageOps, ImageFont, ImageDraw
 
 E_PAPER_WIDTH = 264
 E_PAPER_HEIGHT = 176
-HEADER_SIZE = 20
+
+LEFT_COLUMN_WIDTH = 116
+HEADER_HEIGHT = 30
 
 
 def show_image(black_image, red_image):
@@ -24,14 +26,17 @@ if __name__ == '__main__':
     image_black = Image.new(
         "RGB", (E_PAPER_WIDTH, E_PAPER_HEIGHT), (255, 255, 255))
 
-    image_black_temp = Image.open("./image/map_full.png")
-    image_black.paste(image_black_temp, mask=image_black_temp)
-
     image_red = Image.new(
         "RGB", (E_PAPER_WIDTH, E_PAPER_HEIGHT), (255, 255, 255))
 
+    image_black_temp = Image.open("./image/map_full.png")
+    image_black.paste(image_black_temp, mask=image_black_temp)
+
     image_red_temp = Image.open("./image/map_main-line.png")
     image_red.paste(image_black_temp, mask=image_red_temp)
+
+    image_red.paste(ImageOps.invert(text_to_image.text_to_image(
+        LEFT_COLUMN_WIDTH, HEADER_HEIGHT, u"京急線", 24)), (0, 0))
 
     show_image(image_black.rotate(90, expand=True),
                image_red.rotate(90, expand=True))
